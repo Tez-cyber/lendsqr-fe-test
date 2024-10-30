89e1b956b16273509d474d37e29639c714ead1f7
Salami Azeez <salamitopeazeez@gmail.com>
2024-10-30 18:01:56 +0100

    Completed layout for login page

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,7 +1,40 @@
 
 
 export const LoginPage = () => {
-  return (
-    <div>loginPage</div>
-  )
+    return (
+        <main className="login">
+            {/* =========== Left section with image */}
+            <section className="login_firstSection">
+                <div className="login_firstSection_content">
+                    <div className="logo">
+                        <img src="/logo.svg" alt="" />
+                    </div>
+                    <div className="login_img">
+                        <img src="/login-img.svg" alt="" />
+                    </div>
+                </div>
+            </section>
+            {/* =========== Form section  ========= */}
+            <section className="login_formSection">
+                <div className="login_formSection_title">
+                    <h2>Welcome!</h2>
+                    <p>Enter details to login</p>
+                </div>
+                <form action="" className="login_formSection_form">
+                    <div>
+                        <input type="text" placeholder="Email" />
+                        <input type="text" placeholder="Password" />
+                    </div>
+                    <div>
+                        <span>forgot password?</span>
+                    </div>
+                    <div className="">
+                        <button type='submit'>
+                            log in
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </main>
+    )
 }
